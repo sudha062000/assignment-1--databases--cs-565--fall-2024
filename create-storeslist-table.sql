df659eb3a66983df66ba20afeb42885eeca58d67
Sudharani Gugulothu <sudharani06112@gmail.com>
2024-10-14 18:31:56 -0400

    modified

--- create-storeslist-table.sql
+++ create-storeslist-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS storelist (
-    store_id          VARCHAR(14),
-    store_name        VARCHAR(64),
+    store_id          VARCHAR(14)  NOT NULL,
+    store_name        VARCHAR(64)  NOT NULL,
     comment           TINYTEXT      DEFAULT NULL, -- A comment of up to 255 chars, with each character taking up 1 byte is appropriate. Note that multibyte chars take up at least 2.
     PRIMARY KEY (store_id)
 );
--- create-storeurl-table.sql
+++ create-storeurl-table.sql
@@ -1,1 +1,13 @@
-select
+CREATE TABLE IF NOT EXISTS spices (
+    spice_name        VARCHAR(64)   NOT NULL,     -- Most spice names will fit within a 64-character column.
+    store_id          VARCHAR(14),
+    store_name        VARCHAR(64)   NOT NULL,
+
+    net_weight_in_oz  DECIMAL(5,3)  NOT NULL,     -- For example, 20.125.
+    brand             VARCHAR(48)   NOT NULL,     -- A multiple of 8. Somewhat arbitrary.
+    expiration_date   DATE          DEFAULT NULL, -- Some spices don’t have an expiry date. Thus, a NULL entry is apt.
+    barcode           VARCHAR(14),                -- Barcodes are usually 12 characters in length, but on occasion are as long as 14.
+    comment           TINYTEXT      DEFAULT NULL, -- A comment of up to 255 chars, with each character taking up 1 byte is appropriate. Note that multibyte chars take up at least 2.
+
+    PRIMARY KEY (barcode)
+);
