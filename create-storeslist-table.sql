6f3c56dba67cd2b6deb66069d3963f8c8de69018
Sudharani Gugulothu <sudharani06112@gmail.com>
2024-10-14 18:26:10 -0400

    created storelist table

--- create-storeslist-table.sql
+++ create-storeslist-table.sql
@@ -1,1 +1,7 @@
-select sudha
+CREATE TABLE IF NOT EXISTS storelist (
+ 
+    comment           TINYTEXT      DEFAULT NULL, -- A comment of up to 255 chars, with each character taking up 1 byte is appropriate. Note that multibyte chars take up at least 2.
+    store_id          VARCHAR(14),
+    store_name        VARCHAR(64)
+    PRIMARY KEY (store_id)
+);
